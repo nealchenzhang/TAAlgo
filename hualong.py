a10606d1eeaf68fe55eb67466387386efa43125b
nealzc <nealzc1991@gmail.com>
2019-03-02 10:37:12 +0800

    TODO: hualong

--- hualong.py
+++ hualong.py
@@ -25,6 +25,9 @@ from preprocessing import PB_plotting
 
 from technical_indicators import SMA
 
+from contextlib import closing
+from tqsdk import TqApi, TqSim, TqBacktest, BacktestFinished, TargetPosTask
+
 
 def wave_plotting(ys, Peaks, Bottoms, **kwargs):
     ls_x = ys.index.tolist()
@@ -56,16 +59,14 @@ def wave_plotting(ys, Peaks, Bottoms, **kwargs):
     #        plt.savefig('1.jpg')
     plt.show()
 
-def wave_check(ys, pflag, method='RW', **kwargs):
+
+def wave_check(ys, method='RW', **kwargs):
     l = len(ys)
     if method == 'RW':
         Peaks, Bottoms = RW(ys, w=kwargs['w'], iteration=kwargs['iteration'])
-        Peaks, Bottoms = RW(ys, w=1, iteration=2)
+        # Peaks, Bottoms = RW(ys, w=1, iteration=2)
     elif method == 'TP':
         Peaks, Bottoms = TP(ys, iteration=kwargs['iteration'])
-
-    MA = SMA(ys, w=5)['SMA']
-    wave_plotting(ys, Peaks, Bottoms, MA=MA)
 
     ls_x = ys.index.tolist()
     ls_p = Peaks.index.tolist()
@@ -84,7 +85,7 @@ def wave_check(ys, pflag, method='RW', **kwargs):
     Pot_Index = [0] * m
 
     for i in range(m-1, 0, -1):
-        print(i)
+        # print(i)
         if PB_idx.iloc[i-1: i+1].values.tolist() == Pot_Wave_up1:
             Pot_Index[i-1] = 1
         elif PB_idx.iloc[i-1: i+1].values.tolist() == Pot_Wave_down1:
@@ -96,9 +97,15 @@ def wave_check(ys, pflag, method='RW', **kwargs):
     Wave_up = ys.loc[(PB_idx.iloc[PNidx[-1]:PNidx[-1]+2]).index.tolist()].copy()
     Wave_down = ys.loc[(PB_idx.iloc[PIidx[-1]:PIidx[-1]+2]).index.tolist()].copy()
 
+    MA = SMA(ys, w=5)['SMA']
+    # wave_plotting(ys, Peaks, Bottoms, MA=MA)
+
     dict_results = {
         'Up': Wave_up,
-        'Down': Wave_down
+        'Down': Wave_down,
+        'Peaks': Peaks,
+        'Bottoms': Bottoms,
+        'MA': MA
     }
 
     return dict_results
@@ -126,27 +133,83 @@ def wave_check(ys, pflag, method='RW', **kwargs):
     #     tick.set_rotation(15)
     # plt.show()
 
-    return signal
+
 
 
 if __name__ == '__main__':
 
-    df_ys = pd.read_csv('./Data/IF1903_1min.csv')
-    df_ys.datetime = df_ys.datetime.apply(pd.to_datetime)
-    df_ys.datetime = df_ys.datetime.apply(lambda x: str(x))
-    df_ys.set_index('datetime',inplace=True)
-    ls_cols = df_ys.columns.tolist()
-    str_Close = [i for i in ls_cols if i[-6:] == '.close'][0]
-    ys = df_ys.loc[:, str_Close]
+    # df_ys = pd.read_csv('./Data/IF1903_1min.csv')
+    # df_ys.datetime = df_ys.datetime.apply(pd.to_datetime)
+    # df_ys.datetime = df_ys.datetime.apply(lambda x: str(x))
+    # df_ys.set_index('datetime',inplace=True)
+    # ls_cols = df_ys.columns.tolist()
+    # str_Close = [i for i in ls_cols if i[-6:] == '.close'][0]
+    # ys = df_ys.loc[:, str_Close]
+    #
+    # ys = ys[:40]
+    # dict_results = wave_check(ys, method='RW', w=1, iteration=0)
 
-    ys = ys[:40]
-    
-    
-    MA = SMA(ys, w=20)['SMA']
-    df_tmp = pd.DataFrame(columns=['Close', 'MA'], index=ys.index.tolist())
+    api = TqApi(TqSim(), backtest=TqBacktest(start_dt=dt.datetime(2019,1,2), end_dt=dt.datetime(2019,1,3)))
 
-    df_tmp.loc[:, 'Close'] = ys
-    df_tmp.loc[:, 'MA'] = MA
-    
-    tmp = df_tmp.dropna()
-    ys = tmp[:30]
+    symbol = 'CFFEX.IF1903'
+
+    quote = api.get_quote(symbol)
+    klines = api.get_kline_serial(symbol, duration_seconds=60)
+    target_pos = TargetPosTask(api, symbol)
+    with closing(api):
+        try:
+            while True:
+                api.wait_update()
+                if api.is_changing(klines):
+                    try:
+                        now = dt.datetime.fromtimestamp(klines.datetime[-1]/1e9)
+                    except:
+                        now = 'No data'
+                    # print(now)
+                    ys = pd.Series(data=klines.close[-40:],
+                                   index=[dt.datetime.fromtimestamp(i/1e9) for i in klines.datetime[-40:]]
+                                   )
+                    dict_results = wave_check(ys, 'RW', w=3, iteration=0)
+                    Wave_up = dict_results['Up']
+                    Wave_down = dict_results['Down']
+                    MA = dict_results['MA']
+                    ls_up = Wave_up.index.tolist()
+
+                    price0 = ys[0]
+                    pricet = ys[-1]
+
+                    h = Wave_up[1] - Wave_up[0]
+                else:
+                    pass
+
+                while True:
+                    api.wait_update()
+                    try:
+
+                        if Wave_up[0] < Wave_up[1] and \
+                            pricet > Wave_up[0] and pricet < Wave_up[1] and \
+                            MA.loc[ls_up][0] > Wave_up[0] and MA.loc[ls_up][1] < Wave_up[1] and \
+                            pricet - MA[-1] < 0.02:
+                            print('price target: ', pricet+h)
+                            print('stop loss: ', Wave_up[0])
+                            target_pos.set_target_volume(1)
+                            wave_plotting(ys, Wave_up[1], Wave_up[0])
+                        break
+                    except:
+                        break
+
+                while True:
+                    api.wait_update()
+
+                    if api.is_changing(quote):
+                        if quote['last_price'] > pricet+h:
+                            print('zhiying')
+                            target_pos.set_target_volume(0)
+                        elif pricet <= Wave_up[0]:
+                            print('zhisun')
+                            target_pos.set_target_volume(0)
+                    break
+
+        except BacktestFinished:  # 回测结束, 获取预测结果，统计正确率
+            print("----回测结束----")
+
