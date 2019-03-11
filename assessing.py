1e6d681fc440bfe78c79d3fa36c8dd4b890fa5c3
nealzc <nealzc1991@gmail.com>
2019-03-11 23:41:01 +0800

    pair_test finished

--- assessing.py
+++ assessing.py
@@ -16,44 +16,59 @@ from scipy import stats
 
 # Passive or fixed time periods
 
-
-
-
-
 # Assessing the Performance of Trading Signals
-def pair_tests(ys, h, signal, rtn_type='log'):
+def pair_tests(ys, signal, h=5, rtn_type='log'):
     """
     
     :param ys: original data Series
-    :param h: predefined fixed holding periods
     :param signal: signal Series generated with time index
+    :param h: predefined fixed holding periods
     :param rtn_type: definition of return types
                     'log': logarithmic returns
                     'mean': arithmetic returns
-    :return:
+    :return: h
     """
+    ls_ix = ys.index.tolist()
+    ls_ix_signal = signal.index.tolist()
+    ls_ix_signal_nan = [i for i in ls_ix_signal if i not in signal.dropna().index.tolist()]
     rtn = (ys.shift(h) / ys).apply(np.log)
+    rtn.loc[ls_ix_signal_nan] = np.nan
+    # TODO: check the normality of rtn
 
-    # TODO: refer to null hypothesis test
-    print('# 2. Population Correlation Coefficient Test')
+    rtn_signal = rtn.copy()
+    rtn_signal.loc[signal.where(signal == 0).dropna().index.tolist()] = np.nan
+    rtn_signal.loc[signal.where(signal == 1).dropna().index.tolist()] *= 1
+    rtn_signal.loc[signal.where(signal == -1).dropna().index.tolist()] *= -1
+    rtn_signal.loc[ls_ix_signal_nan] = np.nan
+    rtn_signal.dropna(inplace=True)
+    rtn.dropna(inplace=True)
+
+    print('Null Hypothesis: rtn_signal = rtn')
+    print('Alternative Hypothesis: rtn_signal > rtn')
+    mean_signal, mean_market = rtn_signal.mean(), rtn.mean()
+    std_signal, std_market = rtn_signal.std(ddof=1), rtn.std(ddof=1)
+    n_signal, n_market = rtn_signal.size, rtn.size
+    se_signal, se_market = std_signal / np.sqrt(n_signal), std_market / np.sqrt(n_market)
+
+    sed = np.sqrt(se_signal ** 2 + se_market ** 2)
+    df = n_signal + n_market - 2
+    t_stat = (mean_signal - mean_market) / sed
+
+    # Critical t-value: one-tailed
+    one_tailed_alpha = [0.1, 0.05, 0.01]
     print('-' * 40)
-    print('H0: rho = 0')
-    number = self.data_set.loc[:, x].count()
-    sample_corr = self.data_set.loc[:, [x, y]].corr(method='pearson')[x][y]
-    cal_ttest = (sample_corr * np.sqrt(number - 2)) / np.sqrt(1 - sample_corr ** 2)
-    # Critical t-value: 2-tailed
-    two_tailed_alpha = [0.1, 0.05, 0.01]
-    from scipy import stats
-    print('-' * 40)
-    print('Calculated t_value is {}.\nWith df = {}'.format(cal_ttest, number - 2))
-    for i in two_tailed_alpha:
-        c_t = stats.t.ppf(1 - i / 2, df=number - 2)
-        if (cal_ttest < -c_t) or (cal_ttest > c_t):
-            print('Reject the null hypothesis at the {:.2%} level of significance'.format(i))
+    print('Calculated t_stats is {}.\nWith df = {}'.format(t_stat, df))
+    for alpha in one_tailed_alpha:
+        c_t = stats.t.ppf(1 - alpha, df=df)
+        if t_stat > c_t:
+            print('Reject the null hypothesis at the {:.2%} level of significance'.format(alpha))
+            print('Good to go with fixed {} holding period'.format(h))
         else:
-            print('We failed to reject the null hypothesis at the {:.2%} level of significance'.format(i))
+            print('We failed to reject the null hypothesis at the {:.2%} level of significance'.format(alpha))
 
+    return h
 
+# TODO: Bernoulli trials
 def Bernoulli_trials(p, N):
     """
     p: probability of success: 50%
@@ -82,3 +97,6 @@ if __name__ == '__main__':
     ys = df_ys.loc[:, str_Close]
 
     ys = ys[-300:]
+
+    from technical_indicators import BB
+    signal = BB(ys)['signal']
