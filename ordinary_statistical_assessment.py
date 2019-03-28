ccb4b4bbf23a61df0b67792c6429306b409e1b94
nealzc <nealzc1991@gmail.com>
2019-03-28 23:55:43 +0800

    TODO: B>0 S>0 B-S>0

--- ordinary_statistical_assessment.py
+++ ordinary_statistical_assessment.py
@@ -10,11 +10,46 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 
+from scipy import stats
 
 from technical_indicators import SMA
 
 strategy = SMA
 dict_results = strategy(ys, w=5)
+
+
+def pair_test_of_signal(rtn_signal, rtn, **kwargs):
+
+    print('H0: strategy signal generated return <= unconditional mean daily return')
+
+    mean_signal, mean_market = rtn_signal.mean(), rtn.mean()
+    std_signal, std_market = rtn_signal.std(ddof=1), rtn.std(ddof=1)
+    n_signal, n_market = rtn_signal.size, rtn.size
+    df = n_signal + n_market - 2
+    try:
+        mean_signal = kwargs['mean']
+    except KeyError:
+        pass
+
+    sed = np.sqrt(((n_signal - 1) * std_signal ** 2 + (n_market - 1) * std_market ** 2) / df)
+
+    t_stat = (mean_signal - mean_market) / (sed * np.sqrt(1 / n_signal + 1 / n_market))
+
+    # Critical t-value: one-tailed
+    one_tailed_alpha = [0.1, 0.05, 0.01]
+    print('-' * 40)
+    print('Calculated t_stats is {}.\nWith df = {}'.format(t_stat, df))
+    for alpha in one_tailed_alpha:
+        c_t = stats.t.ppf(1 - alpha, df=df)
+        if t_stat > c_t or t_stat < -c_t:
+            print('Reject the null hypothesis at the {:.2%} level of significance'.format(alpha))
+        else:
+            print('We failed to reject the null hypothesis at the {:.2%} level of significance'.format(alpha))
+
+    # Check p_value
+    pval = stats.t.sf(np.abs(t_stat), df)
+
+    return pval
 
 
 def ordinary_statistical_assessment(dict_results, HP=5):
@@ -74,10 +109,18 @@ def ordinary_statistical_assessment(dict_results, HP=5):
     N_long = position.where(position > 0).count()
     N_short = position.where(position < 0).count()
     log_return = (ys / ys.shift(1)).apply(np.log)
-    Rtn_long = log_return.loc[position.where(position > 0).dropna().index.tolist()].mean()
-    Rtn_short = log_return.loc[position.where(position < 0).dropna().index.tolist()].mean()
-    Rtn_LS = (N_long * Rtn_long - N_short * Rtn_short) / (N_long + N_short)
-    
+    Rtn_long = log_return.loc[position.where(position > 0).dropna().index.tolist()]
+    Rtn_short = log_return.loc[position.where(position < 0).dropna().index.tolist()]
+    Rtn_long_mean = Rtn_long.mean()
+    Rtn_short_mean = Rtn_short.mean()
+
+    Rtn_LS = (Rtn_long.append(-Rtn_short)).sort_index()
+    Rtn_LS_mean = (N_long * Rtn_long_mean - N_short * Rtn_short_mean) / (N_long + N_short)
+
+    p_value_buy = pair_test_of_signal(Rtn_long, log_return)
+    p_value_sell = pair_test_of_signal(Rtn_short, log_return)
+    p_value_BS = pair_test_of_signal(Rtn_LS, log_return, mean=Rtn_LS_mean)
+
     dict_assessment = {
             'N_buy': N_buy_signal,
             'N_sell': N_sell_signal,
